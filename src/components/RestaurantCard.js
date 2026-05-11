54e06f38c8362534c447275122fe63288207ca28
Shrutik <shrutik@gmail.com>
2026-05-11 10:59:42 +0530

    episode-10

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -7,13 +7,13 @@ const RestaurantCard = (props) => {
     resData?.info;
 
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="m-4 p-4 w-59.5 rounded-lg bg-gray-100 hover:bg-gray-200">
       <img
-        className="res-logo"
+        className="w-58 h-34 rounded-lg"
         alt="Restaurant Card Image"
         src={CDN_URL + cloudinaryImageId}
       />
-      <h3>{name}</h3>
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} Stars</h4>
       <h4>{costForTwo}</h4>
